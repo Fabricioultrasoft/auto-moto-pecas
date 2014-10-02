04457dd7c8f964d449177162fc94720951fd197f
nog.junior84@gmail.com <nog.junior84@gmail.com@08c1a579-fb29-31d3-6e0f-84538b1f4620>
2014-10-02 02:26:01 +0000

    Arquivos:
    Includes -> conecta e cabacalho
    Css -> style e bootstrap
    View -> index

---
+++ trunk/SIGAMP/Includes/cabecalho.php
@@ -0,0 +1,9 @@
+<html lang="pt-br">
+    <head>
+        <title>SIGAMP - Sistema de Gerenciamento de Auto Moto Peças</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="../Scripts/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="../Scripts/css/style.css">
+    </head>
+    <body>
+
