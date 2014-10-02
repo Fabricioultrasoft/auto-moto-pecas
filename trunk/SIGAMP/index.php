96aab413e061c5884ad106accd2af825efeb6ea6
nog.junior84@gmail.com <nog.junior84@gmail.com@08c1a579-fb29-31d3-6e0f-84538b1f4620>
2014-10-02 05:05:27 +0000

    Correção do index e mais alguns arquivos css do bootstrap  que estavam faltando

--- trunk/SIGAMP/index.php
+++ trunk/SIGAMP/index.php
@@ -5,8 +5,8 @@ include 'Includes/conecta.php';
     <head>
         <title>SIGAMP - Sistema de Gerenciamento de Auto Moto Peças</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="../Scripts/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="../Scripts/css/style.css">
+        <link rel="stylesheet" type="text/css" href="Scripts/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="Scripts/css/style.css">
     </head>
     
     <body class="login">
