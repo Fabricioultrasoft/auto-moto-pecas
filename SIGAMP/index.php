d86eb94acfa5a415bebe47775e0e6f0eac6eabac
nog.junior84@gmail.com <nog.junior84@gmail.com@08c1a579-fb29-31d3-6e0f-84538b1f4620>
2014-10-17 13:53:39 +0000

    Atualização com templates

--- SIGAMP/index.php
+++ SIGAMP/index.php
@@ -1,29 +1,8 @@
 <?php
-include 'Includes/conecta.php';
+//include 'Includes/conecta.php';
+include 'Includes/cabecalho.php';
 ?>
-<html lang="pt-br">
-    <head>
-        <title>SIGAMP - Sistema de Gerenciamento de Auto Moto Peças</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="Scripts/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="Scripts/css/style.css">
-    </head>
-    
-    <body class="login">
-    <fieldset id="acesso">
-        <h1><font color="black">Acesso ao Sistema</font></h1>
-    <br/><br/>
-    <form action="login.php" method="post">
-        
-        
-            <table border="0" class="table">
-                <tr><td><font color="black">Login</font></td><td><input type="text" name="login" class="form-control" id="form_login" placeholder="Seu nome de usuário"></td></tr>
-                <tr><td><font color="black">Senha</font></td><td><input type="password" name="senha" class="form-control" id="form_login" placeholder="Sua senha de acesso"></td></tr>
-                <tr><td colspan="2"><div align="right"><button class="btn btn-primary">Logar</button></div></td></tr>
-            </table>
-        </fieldset>    
-    </form>
-    </body>
-</html>
-
-
+<br><br><br><br><br><br><br><br><br><br><br><b><br><br>Ola Tudo bem
+<?php
+require 'Includes/rodape.php';
+?>
