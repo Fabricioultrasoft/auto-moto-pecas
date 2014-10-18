b70dca3b226827e9a7fda506e28e5f2cad92248d
nog.junior84@gmail.com <nog.junior84@gmail.com@08c1a579-fb29-31d3-6e0f-84538b1f4620>
2014-10-18 22:49:16 +0000

    Atualização com templates

--- SIGAMP/Includes/rodape.php
+++ SIGAMP/Includes/rodape.php
@@ -1,24 +1,3 @@
-<footer>
-        <div class="padding">
-            <div class="main">
-                <div class="container_12">
-                    <div class="wrapper">
-                        <article class="grid_12">
-                        	<h4 class="indent-bot">Redes Sociais:</h4>
-                            <ul class="list-services border-bot img-indent-bot">
-                            	<li><a href="#">Facebook</a></li>
-                                <li><a class="item-1" href="#">Twitter</a></li>                               
-                            </ul>
-                            <p class="p1">Trabalho Prático do Grupo 4 de Laboratório de Engenharia de Software</p>
-                            <p class="p1">Professor: Rodrigo Lopes Salgado</p>
-                            <p class="p1">Segundo Semestre de 2014</p>
-                            
-                            <!-- {%FOOTER_LINK} -->
-                        </article>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+
 </body>
 </html>
