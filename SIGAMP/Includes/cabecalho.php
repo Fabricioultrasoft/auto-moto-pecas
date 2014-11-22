f91b93b23ccba48f7b76deef0ce15c962ac5d418
nog.junior84@gmail.com <nog.junior84@gmail.com@08c1a579-fb29-31d3-6e0f-84538b1f4620>
2014-11-22 01:48:18 +0000

    Atualizações Venda em andamento

--- SIGAMP/Includes/cabecalho.php
+++ SIGAMP/Includes/cabecalho.php
@@ -4,9 +4,10 @@
     <title>SIGAMP - Sistema Gerenciador de Auto Moto Peças</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="../Scripts/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../Scripts/css/style.css">
     <script type="text/javascript" src="../Scripts/javaScript/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="../Scripts/javaScript/jquery.maskedinput.min.js"/></script>
-  
+    
 
 </head>
 <body>
